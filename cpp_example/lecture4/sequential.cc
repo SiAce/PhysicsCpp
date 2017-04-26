951958dc98132a28d8308651a06dd6d7545f4f61
SiAce <siacespark@gmail.com>
2017-04-26 16:00:21 +0800

    Remove lecture files

--- cpp_example/lecture4/sequential.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-  int n{1}; // declaration statement
-  n = n+1; // expression statement
-  std::cout << "n = " << n << std::endl; // expression statement
-  return 0; // return statement
-}
--- cpp_example/lecture6/vector_scale.cc
+++
@@ -1,9 +0,0 @@
-#include "Vector3D.h"
-
-int main ()
-{
-  Vector3D v1(3, 4, 5);
-  v1.Print();
-  v1.Scale(3);
-  v1.Print();
-}
